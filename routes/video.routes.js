28cea121d8f06c176dd1c373bf443c0083181893
Akshay kumar <akshaykumar880466@gmail.com>
2025-06-10 00:57:10 +0530

    commit init

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -21,6 +21,8 @@ router.post('/upload', checkAuth, async (req, res) => {
 
         }
 
+        // console.log(title,description, req.files.videoUrl)
+
         const videoUpload = await cloudinary.uploader.upload(req.files.videoUrl.tempFilePath, {
             resource_type: "video",
             folder: "Youtube/videos"
@@ -30,6 +32,9 @@ router.post('/upload', checkAuth, async (req, res) => {
             folder: "Youtube/thumbnails"
 
         })
+
+        // console.log(videoUpload);
+        // console.log(thumbnailUpload)
 
         const newVideo = new Video({
             title,
@@ -43,8 +48,9 @@ router.post('/upload', checkAuth, async (req, res) => {
             user_id: req.user.id
 
         })
+        // console.log(newVideo)
 
-        await newVideo.save()
+        await newVideo.save();
 
         res.status(200).json({ message: "video uploaded", newVideo });
     } catch (error) {
@@ -55,46 +61,43 @@ router.post('/upload', checkAuth, async (req, res) => {
 
 
 
-router.put('/update/:id',checkAuth,async(req,res)=>{
+router.put('/update/:id', checkAuth, async (req, res) => {
     try {
-        const {title,description,category,tags}= req.body;
+        const { title, description, category, tags } = req.body;
 
-        const videoId= req.params.id;
+        const videoId = req.params.id;
 
         // find video by id
-        let video= await Video.findById(videoId);
+        let video = await Video.findById(videoId);
         // console.log(video)
 
-        if(!video)
-        {
-            return res.status(404).json({error:"Video not found "})
+        if (!video) {
+            return res.status(404).json({ error: "Video not found " })
         };
 
-        if(video.user_id.toString()!== req.user.id.toString())
-        {
-            return res.status(401).json({error:"UnAuthorized"});
+        if (video.user_id.toString() !== req.user.id.toString()) {
+            return res.status(401).json({ error: "UnAuthorized" });
         }
 
 
-        if(req.files && req.files.thumbnailUrl)
-        {
+        if (req.files && req.files.thumbnailUrl) {
             await cloudinary.uploader.destroy(video.thumbnailId);
 
-            const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnailUrl.tempFilePath,{
-                folder:"Youtube/thumbnails"
+            const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnailUrl.tempFilePath, {
+                folder: "Youtube/thumbnails"
             })
 
-            video.thumbnailUrl= thumbnailUpload.secure_url,
-            video.thumbnailId= thumbnailUpload.public_id
+            video.thumbnailUrl = thumbnailUpload.secure_url,
+                video.thumbnailId = thumbnailUpload.public_id
         }
 
-        video.title= title || video.title,
-        video.description= description || video.description,
-        video.category = category || video.category,
-        video.tags= tags? tags.split(","): video.tags;
+        video.title = title || video.title,
+            video.description = description || video.description,
+            video.category = category || video.category,
+            video.tags = tags ? tags.split(",") : video.tags;
 
         await video.save();
-        res.status(201).json({message:"Updated successfully",video})
+        res.status(201).json({ message: "Updated successfully", video })
 
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -104,9 +107,73 @@ router.put('/update/:id',checkAuth,async(req,res)=>{
 
 // delete Video 
 
-router.delete("/delete/:id", checkAuth,(req,res)=>{
+router.delete("/delete/:id", checkAuth, async (req, res) => {
+    try {
+        const videoId = req.params.id;
 
+        let video = await Video.findById(videoId);
+
+        if (!video) {
+            return res.status(404).json({ error: "Video not found" });
+        }
+
+        console.log(video);
+
+
+        if (video.user_id.toString() !== req.user.id) {
+            return res.status(400).json("UnAuthorized");
+        }
+
+        // Delete from cloudinary 
+        if (video.videoId) {
+            await cloudinary.uploader.destroy(video.videoId, { resource_type: "video" });
+        }
+
+        if (video.thumbnailId) {
+            await cloudinary.uploader.destroy(video.thumbnailId);
+        }
+
+
+        await Video.findByIdAndDelete(videoId)
+
+
+        res.status(200).json({ message: "Video deleted Successfully" });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Video not Deleted  !!", error: error.message });
+    }
 })
+
+
+// Get all video
+router.get("/all", async (req, res) => {
+    try {
+        const video = await Video.find().sort({ createdAt: -1 });
+        res.status(200).json(video);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Somethings went wrong ", message: error.message });
+    }
+})
+
+
+// GET my video
+router.get('/my-videos', checkAuth, async (req, res) => {
+    try {
+        const videos= await Video.findById({user_id:req.body.id}).sort({createdAt:-1});
+
+        res.status(200).json(videos);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Somethings went wrong ", message: error.message });
+    }
+})
+
+
 
 
 export default router;
